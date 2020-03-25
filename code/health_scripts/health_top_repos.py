d61520df937ffe6e4909aafecb4b1b6094f993e1
Dawn M. Foster <dafoster@pivotal.io>
2020-03-25 13:41:51 +0000

    updated to save details in a csv file

--- code/health_scripts/health_top_repos.py
+++ code/health_scripts/health_top_repos.py
@@ -1,4 +1,4 @@
-import csv
+import sys
 from common_functions import augur_db_connect, get_repo_info, get_dates
 from common_functions import sustain_prs_by_repo, contributor_risk, response_time
 from top_repos_common import get_commits_by_repo
@@ -8,6 +8,15 @@ year = 365   # Default to one year of data
 
 engine = augur_db_connect()
 
+# prepare csv file and write header row
+
+try:
+    csv_output = open('output/risk_assessment.csv', 'w')
+    csv_output.write('repo_path,repo_name,repo_id,commits,sustain_risk,sustain_risk_num,contrib_risk,contrib_risk_num,response_risk,response_risk_num\n')
+except:
+    print('Could not write to csv file. Exiting')
+    sys.exit(1)
+
 start_date, end_date = get_dates(year)
 six_start_date, six_end_date = get_dates(six_months)
 commit_threshold = 50
@@ -15,7 +24,6 @@ commit_threshold = 50
 repo_list_commits = get_commits_by_repo(six_start_date, six_end_date, engine)
 
 top = repo_list_commits.loc[repo_list_commits['count'] > commit_threshold]
-
 
 for index, repo in top.iterrows():
 
@@ -25,11 +33,17 @@ for index, repo in top.iterrows():
 
     print(repo_id, repo_name, repo_path, repo['count'])
 
+    repo_info = repo_path + ',' + repo_name + ',' + str(repo_id) + ',' + str(repo['count']) + ','
+    csv_output.write(repo_info)
+
     try:
         sustain_risk_num, sustain_risk = sustain_prs_by_repo(repo_id, repo_name, start_date, end_date, engine)
         contrib_risk_num, contrib_risk = contributor_risk(repo_id, repo_name, start_date, end_date, engine)
         response_risk_num, response_risk = response_time(repo_id, repo_name, start_date, end_date, engine)
+        risk_info = sustain_risk + ',' + str(sustain_risk_num) + ',' + contrib_risk + ',' + str(contrib_risk_num) + ',' + response_risk + ',' + str(response_risk_num) + '\n'
+        csv_output.write(risk_info)
     except:
-        pass
+        csv_output.write(',,,,,\n')
+
 
 
