45ea5452ebdf7c2e449d02b1864c0990d01560b1
Dawn M. Foster <dafoster@pivotal.io>
2020-04-16 10:06:13 +0100

    updated fields to include link to repo and org name instead of repo_path

--- code/health_scripts/inactive_repos.py
+++ code/health_scripts/inactive_repos.py
@@ -8,7 +8,7 @@ engine = augur_db_connect()
 
 try:
     csv_output = open('output/a_repo_activity.csv', 'w')
-    csv_output.write('repo_path,repo_name,repo_id,last_updated,last_commiter,most_commits,most_num,second_most,second_num\n')
+    csv_output.write('repo_link,org,repo_name,repo_id,last_updated,last_commiter,most_commits,most_num,second_most,second_num\n')
 except:
     print('Could not write to csv file. Exiting')
     sys.exit(1)
@@ -31,20 +31,28 @@ all_commits = pd.read_sql_query(all_commits_query, con=engine)
 # Group commits by repo with latest timestamp
 by_repo = all_commits.loc[all_commits.groupby('repo_id').cmt_author_timestamp.idxmax()].sort_values('cmt_author_timestamp')
 
+# Create list of donated orgs to exclude
+
+exclude_list = ["projectcontour", "goharbor"]
+
 # This gives us the top contributors per repo for the csv file 
 
 print('Writing data to CSV file')
 i=0
 for index, row in by_repo.iterrows():
-    basic_info = row.repo_path + ',' + row.repo_name + ',' + str(row.repo_id) + ',' + str(row.cmt_author_timestamp) + ',' + row.cmt_author_email + ','
-    csv_output.write(basic_info)
+    repo_link = row.repo_path + row.repo_name
+    org = row.repo_path.split("/")[1]
+
+    if org not in exclude_list:
+        basic_info = repo_link + ',' + org + ',' + row.repo_name + ',' + str(row.repo_id) + ',' + str(row.cmt_author_timestamp) + ',' + row.cmt_author_email + ','
+        csv_output.write(basic_info)
     
-    top_contribs = all_commits.loc[all_commits['repo_id'] == row.repo_id].cmt_author_email.value_counts()
-    if len(top_contribs) > 1:
-        committer_info = top_contribs.index[0] + ',' + str(top_contribs[0]) + ',' + top_contribs.index[1] + ',' + str(top_contribs[1]) + '\n'
-    elif len(top_contribs) == 1:
-        committer_info = top_contribs.index[0] + ',' + str(top_contribs[0]) + ',' + 'None' + ',' + 'None' + '\n'
-    elif len(top_contribs) == 0:
-        committer_info = 'None' + ',' + 'None' + ',' + 'None' + ',' + 'None' + '\n'
-    csv_output.write(committer_info)
+        top_contribs = all_commits.loc[all_commits['repo_id'] == row.repo_id].cmt_author_email.value_counts()
+        if len(top_contribs) > 1:
+            committer_info = top_contribs.index[0] + ',' + str(top_contribs[0]) + ',' + top_contribs.index[1] + ',' + str(top_contribs[1]) + '\n'
+        elif len(top_contribs) == 1:
+            committer_info = top_contribs.index[0] + ',' + str(top_contribs[0]) + ',' + 'None' + ',' + 'None' + '\n'
+        elif len(top_contribs) == 0:
+            committer_info = 'None' + ',' + 'None' + ',' + 'None' + ',' + 'None' + '\n'
+        csv_output.write(committer_info)
 
