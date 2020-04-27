4113ccfcf6909e7cef0f8fc24d14788d754489a1
Dawn M. Foster <dafoster@pivotal.io>
2020-04-27 11:53:18 +0100

    added field for archived repos

--- code/health_scripts/inactive_repos.py
+++ code/health_scripts/inactive_repos.py
@@ -8,7 +8,7 @@ engine = augur_db_connect()
 
 try:
     csv_output = open('output/a_repo_activity.csv', 'w')
-    csv_output.write('repo_link,org,repo_name,is_fork,last_updated,last_commiter,most_commits,most_num,second_most,second_num\n')
+    csv_output.write('repo_link,org,repo_name,is_fork,is_archived,last_updated,last_commiter,most_commits,most_num,second_most,second_num\n')
 except:
     print('Could not write to csv file. Exiting')
     sys.exit(1)
@@ -47,11 +47,12 @@ for index, row in by_repo.iterrows():
         try:
             repo_api = repo_api_call(row.repo_name, org)
             is_fork = repo_api.fork
+            is_archived = repo_api.archived
 
         except:
             is_fork = 'API ERROR'
 
-        basic_info = repo_link + ',' + org + ',' + row.repo_name + ',' + str(is_fork) + ',' + str(row.cmt_author_timestamp) + ',' + row.cmt_author_email + ','
+        basic_info = repo_link + ',' + org + ',' + row.repo_name + ',' + str(is_fork) + ',' + str(is_archived) + ',' + str(row.cmt_author_timestamp) + ',' + row.cmt_author_email + ','
         csv_output.write(basic_info)
  
         top_contribs = all_commits.loc[all_commits['repo_id'] == row.repo_id].cmt_author_email.value_counts()
